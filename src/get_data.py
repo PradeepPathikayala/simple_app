5e4cffabe5832e0b7ffa9d2cccca428379795cbe
pradeeppathikayala <pradeeppathikayala@outlook.com>
2023-02-17 21:23:11 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -19,12 +19,13 @@ def get_data(config_path):
     data_path = config["data_soucre"]["s3_source"]
 
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    print(df.head())
+    #print(df.head())
+    return df
 
 
 if __name__=='__main__':
     args = argparse.ArgumentParser()
     args.add_argument('--config', default='params.yaml')
     parsed_args = args.parse_args()
-    get_data(config_path=parsed_args.config)
+    data = get_data(config_path=parsed_args.config)
 
