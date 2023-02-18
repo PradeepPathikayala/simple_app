8c1c8710a51ead86eb6f7afe0c4d86128655dbc2
pradeeppathikayala <pradeeppathikayala@outlook.com>
2023-02-18 13:43:54 +0530

    notebook added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,18 @@
+class NotInRange(Exception):
+    def __init__(self, message="value_not_range"):
 
+        #self.input_ = input_
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
     a=2
     b=2
+
+    if a not in range(10,20):
+        raise NotInRange
+    else:
+        pass
     assert a != b
+
+test_generic()
